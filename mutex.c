d78c3314e89b30e9f57a519698b8e20b2a979a91
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-04-26 02:28:06 -0400

    issue

--- mutex.c
+++ mutex.c
@@ -1,18 +1,14 @@
 #include "header.h"
 
-// void    mutex_initialize(t_all *all)
-// {
-//     int index;
+void    mutex_initialize(t_all *all)
+{
+    int index;
 
-
-
-    
-    
-//     index = 0;
-//     while (i < all->p.philo_num)
-//     {
-//         pthread_mutex_init(&all->p.forks[i], NULL);
-//         i++;
-//     }
-// }
+    index = 0;
+    while (i < all->p.philo_num)
+    {
+        pthread_mutex_init(&all->p.forks[i], NULL);
+        i++;
+    }
+}
 
